2bf342061bd53b1b8b6133ba6deca118081323ae
raziuddin49 <mohammadraziuddin49@gmial.com>
2024-09-06 11:54:23 +0530

    edit;getting values from database and filling it to controls

--- TEstApplication/WebForm1.aspx.cs
+++ TEstApplication/WebForm1.aspx.cs
@@ -19,15 +19,29 @@ namespace TEstApplication
             if (Request.QueryString["id"]!= null)
                 
             {
-                string id = Request.QueryString["id"].ToString();
-                SqlConnection cnt = new SqlConnection("Data Source=.;Initial Catalog=db_practice; User id=sa; Password=tuba1234;");
-                string qry = "select u_id, fname,lname,gender,cityid from users where u_id =2";
+                string id = Request.QueryString["id"].ToString();// String requested to string data to put all the queries
+                SqlConnection cnt = new SqlConnection("Data Source=.;Initial Catalog=db_practice; User id=sa; Password=2580;");
+                string qry = "select u_id, fname,lname,gender,cityid from users where u_id ="+Convert.ToInt32(id)+"";
                 DataTable dts = new DataTable();
                 cnt.Open();
                 SqlDataAdapter sqlda = new SqlDataAdapter(qry, cnt);
                 sqlda.Fill(dts);
                 cnt.Close();
-                
+                if (dts != null)
+                {
+                    txtfname.Text = dts.Rows[0]["fname"].ToString();
+                    txtlname.Text = dts.Rows[0]["lname"].ToString();
+                   string gendervalue = dts.Rows[0]["gender"].ToString();
+                    if (gendervalue == "Male")
+                    {
+                        txtgender.SelectedValue = "m";
+                    }
+                    else if (gendervalue == "Female")
+                    {
+                        txtgender.SelectedValue = "f";
+                    }
+                    ddlcity.SelectedValue = dts.Rows[0]["cityid"].ToString();
+                }
 
                
             }
@@ -44,7 +58,7 @@ namespace TEstApplication
             string gender = txtgender.SelectedItem.ToString();
             int cityid = Convert.ToInt32(ddlcity.SelectedValue);
 
-            SqlConnection conn = new SqlConnection("Data Source=.;Initial Catalog=db_practice; User id=sa; Password=tuba1234;");
+            SqlConnection conn = new SqlConnection("Data Source=.;Initial Catalog=db_practice; User id=sa; Password=2580;");
             string query = "insert into users values('"+fname+"', '"+lname+"', '"+gender+"', '"+cityid+"')"; 
             SqlCommand cmd = new SqlCommand(query, conn);
             conn.Open();
@@ -64,7 +78,7 @@ namespace TEstApplication
         }
         private void fillgrid()
         {
-            SqlConnection conn = new SqlConnection("Data Source=.;Initial Catalog=db_practice; User id=sa; Password=tuba1234;");
+            SqlConnection conn = new SqlConnection("Data Source=.;Initial Catalog=db_practice; User id=sa; Password=2580;");
             string select_query = "select u_id, fname, lname, gender, cityid from users";
             DataTable dt = new DataTable();
             conn.Open();
